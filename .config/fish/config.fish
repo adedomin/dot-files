c72904a7a3a1f7168d76cf1cbec795a22d6a9b0e
Anthony DeDominic <adedomin@gmail.com>
2024-12-12 17:17:42 -0500

    vscodium config, fish wip stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,6 +33,26 @@ if status is-interactive
     set --prepend -x PATH "$XDG_CONFIG_HOME/zsh/util-bin:$HOME/.local/bin"
     ## END ENV
 
+    # TODO: WIP automatic host coloring.
+    # set -e fish_color_host
+    # set -e fish_color_host_remote
+    # set -e fish_color_user
+    # # get background color
+    # read \
+    #     --nchars 22 \
+    #     --delimiter / \
+    #     --prompt-str \e']11;?'\a \
+    #     term_bg_red term_bg_green term_bg_blue
+    # # OSC 11 repeats the color twice for some reason? preparing for 10bit+ color?
+    # set term_bg_red 0x(string sub --start=-2 $term_bg_red)
+    # set term_bg_green 0x(string sub --start=-2 $term_bg_green)
+    # set term_bg_blue 0x(string sub --start=-2 $term_bg_blue)
+
+    # function is_light_color -d 'Determine if a color is light'
+    #     test (math \( $argv[1] '*' 299 + $argv[2]'*' 587 + $argv[3] '*' 114 \) / 1000) \
+    #         -gt 125
+    # end
+
     function fish_title
         set -q argv[1]; or set argv (path resolve /proc/self/fd/0)
         echo "$argv"
